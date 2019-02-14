9e311ab8816bde17e3a0a6401344961e1673439a
Kat <m_kaitavia@yahoo.com>
2019-02-14 09:54:04 -0500

    added components

---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Todo from './Todo'
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div>
+                 <h1> Todo List to help you organize</h1>
+                {this.state.item_list.map( item => {
+                    return(<Todo item_name={item}/>)
+                })} 
+            </div>
+        );
+    }
+}
+
+export default TodoList;
